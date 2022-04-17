387adb990559d61a492b8545744af56d940c61ea
D <kritantablake@gmail.com>
2022-04-17 22:50:09 +0300

    Ram DB

---
+++ homework3/RamBD/RamMetricsRepository.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Data.SQLite;
+using System.Globalization;
+using WorkWithBD;
+
+namespace MetricsAgent
+{
+	public class RamMetricsRepository : IRamMetricsRepository
+	{
+        private const string ConnectionString = "Data Source=metrics.db;Version=3;Pooling=true;Max Pool Size=100;";
+
+        public void Create(RamMetrics item)
+        {
+            using var connection = new SQLiteConnection(ConnectionString);
+            connection.Open();
+            using var cmd = new SQLiteCommand(connection);
+            cmd.CommandText = "INSERT INTO rammetrics(value, time) VALUES(@value,@time)";
+            cmd.Parameters.AddWithValue("@value", item.Value);
+            cmd.Parameters.AddWithValue("@time", item.Time.ToString("s", CultureInfo.GetCultureInfo("ru-RU")));
+            cmd.Prepare();
+            cmd.ExecuteNonQuery();
+        }
+
+
+        public void Delete(int id)
+        {
+            using var connection = new SQLiteConnection(ConnectionString);
+            connection.Open();
+            using var cmd = new SQLiteCommand(connection);
+            cmd.CommandText = "DELETE FROM rammetrics WHERE id=@id";
+            cmd.Parameters.AddWithValue("@id", id);
+            cmd.Prepare();
+            cmd.ExecuteNonQuery();
+        }
+
+
+        public void Update(RamMetrics item)
+        {
+            using var connection = new SQLiteConnection(ConnectionString);
+            connection.Open();
+            using var cmd = new SQLiteCommand(connection);
+            cmd.CommandText = "UPDATE rammetrics SET value = @value, time = @time WHERE id=@id;";
+            cmd.Parameters.AddWithValue("@id", item.Id); cmd.Parameters.AddWithValue("@value", item.Value); cmd.Parameters.AddWithValue("@time", item.Time); cmd.Prepare();
+            cmd.ExecuteNonQuery();
+        }
+
+
+        public IList<RamMetrics> GetAll()
+        {
+            using var connection = new SQLiteConnection(ConnectionString);
+            connection.Open();
+            using var cmd = new SQLiteCommand(connection);
+            cmd.CommandText = "SELECT * FROM rammetrics";
+            var returnList = new List<RamMetrics>();
+            using (SQLiteDataReader reader = cmd.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    returnList.Add(new RamMetrics
+                    {
+                        Id = reader.GetInt32(0),
+                        Value = reader.GetInt32(1),
+                        Time = reader.GetDateTime(2)
+                    });
+                    Console.WriteLine(returnList[^1].Time);
+                }
+            }
+            return returnList;
+
+        }
+
+
+        public RamMetrics GetById(int id)
+        {
+            using var connection = new SQLiteConnection(ConnectionString);
+            connection.Open();
+            using var cmd = new SQLiteCommand(connection);
+            cmd.CommandText = "SELECT * FROM rammetrics WHERE id= @id";
+            cmd.Parameters.AddWithValue("@id", id);
+            using (SQLiteDataReader reader = cmd.ExecuteReader())
+            {
+                if (reader.Read())
+                {
+                    return new RamMetrics
+                    {
+                        Id = reader.GetInt32(0),
+                        Value = reader.GetInt32(1),
+                        Time = DateTime.Now - TimeSpan.FromDays(reader.GetInt32(1))   //иного решения не нашел
+                    };
+                }
+                else
+                {
+                    return null;
+                }
+            }
+        }
+    }
+}
+
