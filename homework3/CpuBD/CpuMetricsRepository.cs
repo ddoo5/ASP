e48695d77b8be3c43b3d7ce74e31a3e1cd9e3852
D <kritantablake@gmail.com>
2022-04-17 22:49:03 +0300

    Cpu DB

---
+++ homework3/CpuBD/CpuMetricsRepository.cs
@@ -0,0 +1,97 @@
+﻿using System.Data.SQLite;
+using System.Globalization;
+
+namespace WorkWithBD
+{
+    public class CpuMetricsRepository : ICpuMetricsRepository
+    {
+        private const string ConnectionString = "Data Source=metrics.db;Version=3;Pooling=true;Max Pool Size=100;";
+
+
+        public void Create(CpuMetrics item)
+        {
+            using var connection = new SQLiteConnection(ConnectionString);
+            connection.Open();
+            using var cmd = new SQLiteCommand(connection);
+            cmd.CommandText = "INSERT INTO cpumetrics(value, time) VALUES(@value,@time)";
+            cmd.Parameters.AddWithValue("@value", item.Value);
+            cmd.Parameters.AddWithValue("@time", item.Time.ToString("s", CultureInfo.GetCultureInfo("ru-RU")));
+            cmd.Prepare();
+            cmd.ExecuteNonQuery();
+        }
+
+
+        public void Delete(int id)
+        {
+            using var connection = new SQLiteConnection(ConnectionString);
+            connection.Open();
+            using var cmd = new SQLiteCommand(connection);
+            cmd.CommandText = "DELETE FROM cpumetrics WHERE id=@id";
+            cmd.Parameters.AddWithValue("@id", id);
+            cmd.Prepare();
+            cmd.ExecuteNonQuery();
+        }
+
+
+        public void Update(CpuMetrics item)
+        {
+            using var connection = new SQLiteConnection(ConnectionString);
+            connection.Open();
+            using var cmd = new SQLiteCommand(connection);
+            cmd.CommandText = "UPDATE cpumetrics SET value = @value, time = @time WHERE id=@id;";
+            cmd.Parameters.AddWithValue("@id", item.Id); cmd.Parameters.AddWithValue("@value", item.Value); cmd.Parameters.AddWithValue("@time", item.Time); cmd.Prepare();
+            cmd.ExecuteNonQuery();
+        }
+
+
+        public IList<CpuMetrics> GetAll()
+        {
+            using var connection = new SQLiteConnection(ConnectionString);
+            connection.Open();
+            using var cmd = new SQLiteCommand(connection);
+            cmd.CommandText = "SELECT * FROM cpumetrics";
+            var returnList = new List<CpuMetrics>();
+            using (SQLiteDataReader reader = cmd.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    returnList.Add(new CpuMetrics
+                    {
+                        Id = reader.GetInt32(0),
+                        Value = reader.GetInt32(1),
+                        Time = reader.GetDateTime(2)
+                    });
+                    Console.WriteLine(returnList[^1].Time);
+                }
+            }
+            return returnList;
+        }
+
+
+        public CpuMetrics GetById(int id)
+        {
+            using var connection = new SQLiteConnection(ConnectionString);
+            connection.Open();
+            using var cmd = new SQLiteCommand(connection);
+            cmd.CommandText = "SELECT * FROM cpumetrics WHERE id= @id";
+            cmd.Parameters.AddWithValue("@id", id);
+            using (SQLiteDataReader reader = cmd.ExecuteReader())
+            {
+                if (reader.Read())
+                {
+                    return new CpuMetrics
+                    {
+                        Id = reader.GetInt32(0),
+                        Value = reader.GetInt32(1),
+                        Time = DateTime.Now - TimeSpan.FromDays(reader.GetInt32(1))   //иного решения не нашел
+                    };
+                }
+                else
+                {
+                    return null;
+                }
+            }
+        }
+    }
+}
+
