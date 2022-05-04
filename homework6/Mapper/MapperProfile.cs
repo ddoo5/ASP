b3df35592d2d50fde1eb38f043452adc65fac738
D <kritantablake@gmail.com>
2022-05-05 02:30:05 +0300

    mapper

---
+++ homework6/Mapper/MapperProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using MManager.Dto;
+using MManager.Models;
+
+namespace MetricsAgent.Mapper
+{
+	public class MapperProfile : Profile
+	{
+		public MapperProfile()
+		{
+			CreateMap<CpuMetric, CpuMetricDTO>();
+			CreateMap<RamMetric, RamMetricDTO>();
+			CreateMap<HddMetric, HddMetricDTO>();
+			CreateMap<DotNetMetric, DotNetMetricDTO>();
+			CreateMap<NetWorkMetric, NetWorkMetricDTO>();
+		}
+	}
+}
+
