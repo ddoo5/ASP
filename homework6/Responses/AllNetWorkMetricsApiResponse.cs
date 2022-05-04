78bd1172d7df2ae3de042abc6336c8c942581e0c
D <kritantablake@gmail.com>
2022-05-05 02:29:55 +0300

    response

---
+++ homework6/Responses/AllNetWorkMetricsApiResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace MManager.Responses
+{
+    public class AllNetWorkMetricsApiResponse
+    {
+        public DateTimeOffset Time { get; set; }
+        public int Value { get; set; }
+        public int Id { get; set; }
+    }
+}
