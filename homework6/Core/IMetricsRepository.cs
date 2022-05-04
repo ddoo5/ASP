176c25a524f55195ac3ebec2adccee26513c46bd
D <kritantablake@gmail.com>
2022-05-05 02:30:45 +0300

    забытый metric repository

---
+++ homework6/Core/IMetricsRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace MManager.Repo
+{
+     public interface IMetricsRepository<T> where T : class
+    {
+        void AddMetric(T item);
+        IList<T> GetAgentMetricById(int id);
+        IList<T> GetMetricsFromAllCluster();
+        IList<T> GetMetricsByTimePeriod(DateTimeOffset item, DateTimeOffset item2);
+        DateTimeOffset GetLastMetric(long id);
+    }
+}
