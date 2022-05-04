e207b589ff033f6ceb966ebf4f2d85fe178a770c
D <kritantablake@gmail.com>
2022-05-05 02:29:40 +0300

    request

---
+++ homework6/Requests/GetAllCpuMetricsApiRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace MManager.AgentRequest
+{
+    public class GetAllCpuMetricsApiRequest
+    {
+        public string ClientBaseAddress  { get; set; }
+        public DateTimeOffset fromTime { get; set; }
+        public DateTimeOffset toTime { get; set; }
+    }
+}
