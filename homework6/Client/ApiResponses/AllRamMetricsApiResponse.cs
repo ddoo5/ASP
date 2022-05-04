9661c50b5674085c47a404e34791cc9535a570d6
D <kritantablake@gmail.com>
2022-05-05 02:27:32 +0300

    api response

---
+++ homework6/Client/ApiResponses/AllRamMetricsApiResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace MetricsAgent.Api
+{
+    public class AllRamMetricsApiResponse
+    {
+        public int Id { get; set; }
+        public int Value { get; set; }
+        public DateTime Time { get; set; }
+    }
+}
