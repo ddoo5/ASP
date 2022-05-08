5fe24a04ec0f0babe6bb156fc9e825051a4dcfa5
D <kritantablake@gmail.com>
2022-05-08 17:24:39 +0300

    MAgent

---
+++ full/Controllers/DotNetController.cs
@@ -0,0 +1,116 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using WorkWithBD;
+using Microsoft.OpenApi.Models;
+
+namespace Metrics.Controllers
+{
+    [Route("api/metric/dotnet")]
+    [ApiController]
+    public class DotNetController : ControllerBase
+    {
+        private IDotNetMetricsRepository repository;
+        private readonly ILogger<DotNetController> _logger;
+        private readonly IMapper _mapper;
+
+
+        public DotNetController(ILogger<DotNetController> logger, IDotNetMetricsRepository repository, IMapper mapper)
+        {
+            _logger = logger;
+            _logger.LogDebug(1, "NLog entered in DotNetController");
+            this.repository = repository;
+            this._mapper = mapper;
+            _logger.LogDebug(1, "Mapper entered in DotNetController");
+        }
+
+
+
+        [HttpPost("create")]
+        public IActionResult Create([FromBody] DotNetMetricCreateRequest request)
+        {
+            _logger.LogInformation(1, $"Entered request: Value: {request.Value},\n Time: {request.Time}");
+            repository.Create(new DotNetMetrics
+            {
+                Time = request.Time,
+                Value = request.Value
+            });
+            return Ok("Created");
+        }
+
+
+        [HttpGet("all")]
+        public IActionResult GetAll()
+        {
+            _logger.LogInformation(1, $"Entered request: Display dotnetmetrics database");
+            var config = new MapperConfiguration(cfg => cfg.CreateMap<DotNetMetrics, DotNetDto>());
+            var mapper = config.CreateMapper();
+            IList<DotNetMetrics> metrics = repository.GetAll();
+            var response = new AllDotNetMetricsResponse()
+            {
+                Metrics = new List<DotNetDto>()
+            };
+            foreach (var metric in metrics)
+            {
+                response.Metrics.Add(mapper.Map<DotNetDto>(metric));
+            }
+            foreach (var metric in metrics)
+            {
+                response.Metrics.Add(new DotNetDto { Id = metric.Id, Value = metric.Value, Time = metric.Time });
+                _logger.LogInformation(2, $"Displayed: {metric.Id}, {metric.Value}, {metric.Time}");
+            }
+            return Ok(response);
+        }
+
+
+        [HttpPost("delete")]
+        public IActionResult Delete([FromQuery] int id)
+        {
+            _logger.LogInformation(1, $"Entered request: Delete {id} item in dotnetmetrics database");
+            repository.Delete(id);
+            return Ok("Deleted");
+        }
+
+
+        [HttpPost("update")]
+        public IActionResult Update([FromBody] DotNetMetrics metric)
+        {
+            _logger.LogInformation(1, $"Entered request: Update dotnetmetrics database");
+            var metrics = repository.GetAll();
+            foreach (var metricq in metrics)
+            {
+                if (metricq.Id == metric.Id)
+                {
+                    _logger.LogInformation(2, $"From: {metricq.Id}  {metricq.Value}  {metricq.Time}");
+                }
+            }
+            _logger.LogInformation(2, $"To: {metric.Id}  {metric.Value}  {metric.Time}");
+            repository.Update(metric);
+            return Ok("Updated");
+        }
+
+
+        [HttpPost("getbyid")]
+        public IActionResult GetById([FromQuery] int id)
+        {
+            _logger.LogInformation(1, $"Entered request: Display {id} item from dotnetmetrics database");
+            var metrics = repository.GetById(id);
+            return Ok(metrics);
+        }
+
+
+        [HttpGet("api/metrics/dotnet/errors-count/from/{fromTime}/to/{toTime}")]
+        public IActionResult GetMetricsFromAgent( [FromRoute] DateTime fromTime, [FromRoute] DateTime toTime)
+        {
+            IList<DotNetMetrics> metrics = repository.GetMetricsByTimePeriod(fromTime, toTime);
+            List<DotNetDto> Metrics = new List<DotNetDto>();
+
+            foreach (var metric in metrics)
+            {
+                Metrics.Add(_mapper.Map<DotNetDto>(metric));
+
+            }
+            return Ok(Metrics);
+        }
+    }
+}
+
---
+++ full/Program.cs
@@ -0,0 +1,92 @@
+﻿using MetricsAgent;
+using NLog.Web;
+using WorkWithBD;
+using AutoMapper;
+using FluentMigrator.Runner;
+using Quartz;
+using Quartz.Spi;
+using Quartz.Impl;
+
+var builder = WebApplication.CreateBuilder(args);
+var _logger = NLogBuilder.ConfigureNLog("nlog.config").GetCurrentClassLogger();
+var mapperConfiguration = new MapperConfiguration(mp => mp.AddProfile(new MapperProfile()));
+var _mapper = mapperConfiguration.CreateMapper();
+
+_logger.Debug("Run");
+
+
+try
+{
+    builder.Logging.ClearProviders();
+    builder.Logging.SetMinimumLevel(Microsoft.Extensions.Logging.LogLevel.Trace);
+    builder.Host.UseNLog();
+
+    builder.Services.AddControllers();
+
+    builder.Services.AddHostedService<QuartzHostedService>();
+
+    builder.Services.AddFluentMigratorCore()
+                .ConfigureRunner(rb => rb
+                    .AddSQLite()
+                    .WithGlobalConnectionString("Data Source=metrics.db")
+                    .ScanIn(typeof(Program).Assembly).For.Migrations())
+                .AddLogging(lb => lb.AddFluentMigratorConsole());
+
+    builder.Services.AddSingleton<ICpuMetricsRepository, CpuMetricsRepository>();
+    builder.Services.AddSingleton<INetworkMetricsRepository, NetworkMetricsRepository>();
+    builder.Services.AddSingleton<IRamMetricsRepository, RamMetricsRepository>();
+    builder.Services.AddSingleton<IDotNetMetricsRepository, DotNetMetricsRepository>();
+    builder.Services.AddSingleton<IHddMetricsRepository, HddMetricsRepository>();
+
+    builder.Services.AddSingleton<IJobFactory, SingletonJobFactory>();
+    builder.Services.AddSingleton<ISchedulerFactory, StdSchedulerFactory>();
+
+    builder.Services.AddSingleton<CpuMetricJob>();
+    builder.Services.AddSingleton(new JobSchedule(jobType: typeof(CpuMetricJob), cronExpression: "0/2 * * * * ?"));
+
+    builder.Services.AddSingleton<RamMetricJob>();
+    builder.Services.AddSingleton(new JobSchedule(jobType: typeof(RamMetricJob), cronExpression: "0/2 * * * * ?"));
+
+    builder.Services.AddSingleton<NetworkMetricJob>();
+    builder.Services.AddSingleton(new JobSchedule(jobType: typeof(NetworkMetricJob), cronExpression: "0/2 * * * * ?"));
+
+    builder.Services.AddSingleton<HddMetricJob>();
+    builder.Services.AddSingleton(new JobSchedule(jobType: typeof(HddMetricJob), cronExpression: "0/2 * * * * ?"));
+
+    builder.Services.AddSingleton<DotNetMetricJob>();
+    builder.Services.AddSingleton(new JobSchedule(jobType: typeof(DotNetMetricJob), cronExpression: "0/2 * * * * ?"));
+
+    builder.Services.AddSingleton(_mapper);
+
+    builder.Services.AddEndpointsApiExplorer();
+
+    builder.Services.AddSwaggerGen();
+
+    var app = builder.Build();
+
+    using (var scope = app.Services.CreateScope())
+    {
+        scope.ServiceProvider.GetRequiredService<IMigrationRunner>().MigrateUp();
+    }
+
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+
+    app.UseAuthorization();
+
+    app.MapControllers();
+
+    app.Run();
+}
+catch (Exception exception)
+{
+    _logger.Error(exception, "Stopped program because of exception");
+    throw;
+}
+finally
+{
+    NLog.LogManager.Shutdown();
+}
